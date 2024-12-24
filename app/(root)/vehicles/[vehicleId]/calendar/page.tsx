fa1e558991d106c13fb78d3029f1a2fbe9b43f58
othegagan <hellothegagan@gmail.com>
2024-12-24 11:18:31 +0530

    chore: fix Vehicle Unavailability in calendar

--- app/(root)/vehicles/[vehicleId]/calendar/page.tsx
+++ app/(root)/vehicles/[vehicleId]/calendar/page.tsx
@@ -1,31 +1,30 @@
 'use client';
 
-import { Skeleton } from '@/components/skeletons';
 import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/extension/field';
 import MonthPicker from '@/components/ui/month-picker';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { QUERY_KEYS } from '@/constants/query-keys';
-import { useChannels } from '@/hooks/useChannels';
 import { useVehicleFeaturesById, useVehicleTripById } from '@/hooks/useVehicles';
 import { convertToTimeZoneISO, formatDateAndTime } from '@/lib/utils';
+import { deleteUnavailabilityById, insertUnavailability } from '@/server/dynamicPricingAndUnavailability';
 import '@/styles/fullcalendar.css';
-import { deleteUnavailabilityById } from '@/server/dynamicPricingAndUnavailability';
 import type FullCalendar from '@fullcalendar/react';
 import { useQueryClient } from '@tanstack/react-query';
-import { endOfMonth, format, startOfMonth } from 'date-fns';
+import { addDays, endOfMonth, format, startOfMonth } from 'date-fns';
 import { Trash2 } from 'lucide-react';
+import { getSession } from 'next-auth/react';
 import { useParams } from 'next/navigation';
 import { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import CalendarComponent from '../../_components/CalendarComponent';
-import AddUnavailabilityForm from '../../_components/unavailability/AddUnavailabilityForm';
-import UpdateUnavailabilityForm from '../../_components/unavailability/UpdateUnavailabilityForm';
+import { DateRangeCalendar } from '../../_components/DateRangeCalendar';
 
 export default function VehicleCalendarPage() {
     const { vehicleId } = useParams();
     const queryClient = useQueryClient();
 
-    const [selectedChannel, setSelectedChannel] = useState('1');
+    // const [selectedChannel, setSelectedChannel] = useState('1');
     const [currentMonth, setCurrentMonth] = useState(new Date());
     const [startDate, setStartDate] = useState(format(startOfMonth(currentMonth), 'yyyy-MM-dd'));
     const [endDate, setEndDate] = useState(format(endOfMonth(currentMonth), 'yyyy-MM-dd'));
@@ -67,10 +66,10 @@ export default function VehicleCalendarPage() {
         Number(vehicleId)
     );
 
-    const { data: channelsResponse, isLoading: isLoadingChannels, error: errorChannels } = useChannels();
+    // const { data: channelsResponse, isLoading: isLoadingChannels, error: errorChannels } = useChannels();
 
-    const isLoading = isLoadingTrips || isLoadingFeatures || isLoadingChannels;
-    const error = errorTrips || errorFeatures || errorChannels;
+    const isLoading = isLoadingTrips || isLoadingFeatures;
+    const error = errorTrips || errorFeatures;
 
     const tripsData = tripsResponse?.data?.activetripresponse || [];
 
@@ -81,47 +80,20 @@ export default function VehicleCalendarPage() {
 
     const vehiclePricePerDay = featuresResponse?.data?.vehicleAllDetails[0]?.price_per_hr || 0;
 
-    const channelsData = channelsResponse?.data?.channels || [];
+    // const channelsData = channelsResponse?.data?.channels || [];
 
     const vin = featuresResponse?.data?.vehicleAllDetails[0]?.vin || '';
 
     return (
         <div className='flex flex-col'>
             <div className='mb-4 flex w-full gap-6 p-0.5'>
-                {isLoadingChannels ? (
-                    <div className='flex items-center justify-between gap-5'>
-                        <Skeleton className='h-9 w-[150px] rounded-full' />
-                        <Skeleton className='h-9 w-[150px] rounded-full' />
-                    </div>
-                ) : (
-                    <>
-                        <MonthPicker
-                            currentMonth={currentMonth}
-                            onMonthChange={handleMonthChange}
-                            setCurrentMonth={setCurrentMonth}
-                            // calendarRef={calendarRef}
-                            className='w-[200px]'
-                        />
-                        <Select
-                            onValueChange={(value) => {
-                                setSelectedChannel(value);
-                                refetchData();
-                            }}
-                            value={selectedChannel}
-                            defaultValue={selectedChannel}>
-                            <SelectTrigger className='max-w-[150px] rounded-full'>
-                                <SelectValue placeholder='Select channel' />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {channelsData.map((item: any) => (
-                                    <SelectItem key={item.id} value={String(item.id)} className='capitalize'>
-                                        {item.channelName}
-                                    </SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
-                    </>
-                )}
+                <MonthPicker
+                    currentMonth={currentMonth}
+                    onMonthChange={handleMonthChange}
+                    setCurrentMonth={setCurrentMonth}
+                    // calendarRef={calendarRef}
+                    className='w-[200px]'
+                />
             </div>
 
             {isLoading && <div className='flex h-full w-full items-center justify-center'>Loading...</div>}
@@ -172,6 +144,60 @@ function UnavailabilityComponent({
     unavailabilityData: any[];
     zipcode: string;
 }) {
+    const [isAdding, setIsAdding] = useState(false);
+    const [startDate, setStartDate] = useState(format(new Date(), 'yyyy-MM-dd'));
+    const [endDate, setEndDate] = useState(format(addDays(new Date(), 1), 'yyyy-MM-dd'));
+
+    const {
+        handleSubmit,
+        reset,
+        formState: { isSubmitting }
+    } = useForm({
+        mode: 'onChange'
+    });
+
+    function startAdding() {
+        setIsAdding(true);
+    }
+
+    function cancelAdding() {
+        setIsAdding(false);
+        resetForm();
+    }
+
+    function resetForm() {
+        setStartDate(format(new Date(), 'yyyy-MM-dd'));
+        setEndDate(format(addDays(new Date(), 1), 'yyyy-MM-dd'));
+        reset();
+    }
+
+    async function onSubmit() {
+        try {
+            const session = await getSession();
+            const payload = {
+                day: 0,
+                vin,
+                vehicleid: vehicleId,
+                hostid: session?.iduser,
+                repeattype: 0,
+                startdate: convertToTimeZoneISO(`${startDate}T00:00:00`, zipcode),
+                enddate: convertToTimeZoneISO(`${endDate}T23:59:59`, zipcode)
+            };
+
+            const response = await insertUnavailability(payload);
+
+            if (response.success) {
+                refetchData();
+                cancelAdding();
+                toast.success(response.message);
+            } else {
+                toast.error(response.message);
+            }
+        } catch (error: unknown) {
+            console.error('Error adding unavailability:', error);
+        }
+    }
+
     async function deleteUnavailability(id: number) {
         toast.promise(deleteUnavailabilityById(id), {
             loading: 'Deleting Unavailability...',
@@ -186,11 +212,35 @@ function UnavailabilityComponent({
     }
 
     return (
-        <div className='flex w-full flex-col gap-4 '>
+        <div className='flex w-full flex-col gap-4'>
             <div className='flex w-full items-center justify-between'>
-                <h4 className='text-nowrap text-lg'> Vehicle Unavailability</h4>
-                <AddUnavailabilityForm vin={vin} vehicleId={vehicleId} refetchData={refetchData} zipcode={zipcode} />
+                <h4 className='text-nowrap text-lg'>Vehicle Unavailability</h4>
+                {!isAdding ? (
+                    <Button variant='black' className='w-fit' onClick={startAdding}>
+                        Add
+                    </Button>
+                ) : null}
             </div>
+
+            {isAdding && (
+                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 rounded-md border p-4'>
+                    <div className='flex max-w-full flex-col gap-2'>
+                        <Label htmlFor='date-range' className='font-medium'>
+                            Date Range
+                        </Label>
+                        <DateRangeCalendar startDate={startDate} endDate={endDate} setStartDate={setStartDate} setEndDate={setEndDate} />
+                    </div>
+                    <div className='ml-auto flex gap-2'>
+                        <Button variant='black' type='submit' loading={isSubmitting} loadingText='Saving...'>
+                            Save
+                        </Button>
+                        <Button variant='outline' onClick={cancelAdding}>
+                            Cancel
+                        </Button>
+                    </div>
+                </form>
+            )}
+
             <div>
                 {unavailabilityData.map((item: any) => (
                     <div key={item.vinunavailableid} className='my-2 flex items-center justify-between gap-4 border-b pb-1.5 text-14'>
@@ -199,21 +249,8 @@ function UnavailabilityComponent({
                             {formatDateAndTime(item.enddate, zipcode, 'MMM DD, YYYY')}
                         </div>
                         <div className='flex items-center gap-2'>
-                            <UpdateUnavailabilityForm
-                                refetchData={refetchData}
-                                zipcode={zipcode}
-                                hostId={item.hostId}
-                                dbStartDate={item.startdate}
-                                dbEndDate={item.enddate}
-                                dbId={item.vinunavailableid}
-                            />
-
-                            <Button
-                                variant='secondary'
-                                size='icon'
-                                className='h-auto w-auto p-1.5'
-                                onClick={() => deleteUnavailability(item.vinunavailableid)}>
-                                <Trash2 className='size-4' />
+                            <Button variant='secondary' onClick={() => deleteUnavailability(item.vinunavailableid)}>
+                                <Trash2 className='size-4 text-destructive' />
                             </Button>
                         </div>
                     </div>
