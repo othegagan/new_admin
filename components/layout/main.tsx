2fa4d92ee668083ccaa0de26ebd4e8880c0562b7
othegagan <hellothegagan@gmail.com>
2024-12-24 19:16:35 +0530

    chore: update padding for main layout

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -5,7 +5,9 @@ interface MainProps extends React.HTMLAttributes<React.ComponentRef<'main'>> {
     fixed?: boolean;
 }
 
-export const Main = React.forwardRef<React.ComponentRef<'main'>, MainProps>(({ fixed, ...props }, ref) => {
-    return <main ref={ref} className={cn('px-4 py-6 ', fixed && 'flex flex-grow flex-col overflow-hidden')} {...props} />;
+export const Main = React.forwardRef<React.ComponentRef<'main'>, MainProps>(({ fixed, className, ...props }, ref) => {
+    return (
+        <main ref={ref} className={cn('px-4 py-6 md:pl-6 ', fixed && 'flex flex-grow flex-col overflow-hidden', className)} {...props} />
+    );
 });
 Main.displayName = 'Main';
