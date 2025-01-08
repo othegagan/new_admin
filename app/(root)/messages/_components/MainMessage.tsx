52edada995b0b8b2f3bbb4ef778849c7eb3bbb10
othegagan <hellothegagan@gmail.com>
2025-01-08 14:12:56 +0530

    fix: improve text wrapping for message display in MainMessage component

--- app/(root)/messages/_components/MainMessage.tsx
+++ app/(root)/messages/_components/MainMessage.tsx
@@ -265,7 +265,7 @@ function MessageItem({
                 )}
 
                 <div className='flex flex-col gap-2 rounded-lg rounded-tl-none bg-[#E1EFFE] px-3 py-2 dark:bg-[#2e4161]'>
-                    <span className='text-xs'> {message.message}</span>
+                    <span className='overflow-wrap-anywhere word-break-break-word break-words text-xs'>{message.message}</span>
 
                     {message.mediaUrl && (
                         <ImagePreview
@@ -286,7 +286,7 @@ function MessageItem({
     if (isHostMessage) {
         return (
             <div className='ml-auto flex w-max max-w-[75%] flex-col gap-2 rounded-lg rounded-br-none bg-primary/40 px-3 py-2'>
-                <span className='text-xs'> {message.message}</span>
+                <span className='overflow-wrap-anywhere word-break-break-word break-words text-xs'>{message.message}</span>
 
                 {message?.mediaUrl && (
                     <ImagePreview
@@ -314,7 +314,7 @@ function MessageItem({
 
             {message.message.toLocaleLowerCase() === 'a new reservation was requested' ? (
                 <div className='flex flex-col gap-2 rounded-lg bg-muted px-3 py-2 text-sm'>
-                    <span className='text-xs'> {message.message}</span>
+                    <span className='overflow-wrap-anywhere word-break-break-word break-words'>{message.message}</span>
                     {/*
                     {images.length > 0 ? (
                         <div className='relative max-w-md sm:overflow-hidden md:max-w-lg md:rounded-lg'>
@@ -351,8 +351,8 @@ function MessageItem({
                     </p>
                 </div>
             ) : (
-                <div className='flex flex-col gap-2 rounded-lg rounded-tl-none bg-muted px-3 py-2 text-xs'>
-                    {message.message}
+                <div className='overflow-wrap-anywhere word-break-break-word flex max-w-[75%] flex-col gap-2 break-words rounded-lg rounded-tl-none bg-muted px-3 py-2 text-xs'>
+                    <span className='overflow-wrap-anywhere word-break-break-word break-words'>{message.message}</span>
 
                     {message.mediaUrl && <img src={message.mediaUrl} alt='media content' className='mt-2 h-auto max-w-full rounded-lg' />}
                     <p className='flex items-center justify-end text-[10px] text-muted-foreground'>
