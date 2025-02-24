06fdeade223977fb7e3b0b6f4c486529befdc1f2
othegagan <hellothegagan@gmail.com>
2025-02-24 17:21:56 +0530

    fix: add buttons back in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,9 @@
 import { AppSidebar } from '@/components/layout/app-sidebar';
+import ActionRequiredButton from '@/components/layout/header/action-required-button';
 import { Header } from '@/components/layout/header/app-header';
+import { MessagesNotificationButton } from '@/components/layout/header/messages-notification-button';
+import { NotificationBellButton } from '@/components/layout/header/notification-bellbutton';
+import { ProfileDropdown } from '@/components/layout/header/profile-dropdown';
 import Logo from '@/components/layout/logo';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { getNavItems } from '@/constants';
@@ -28,11 +32,11 @@ export default async function RouteComponent({ children }: { children: React.Rea
                 <Header sticky>
                     <Logo className='-ml-3 w-[72px] pt-2 md:hidden md:w-24' herf={PAGE_ROUTES.DASHBOARD} />
                     <div className='ml-auto flex items-center gap-3'>
-                        {/* <ActionRequiredButton />
+                        <ActionRequiredButton />
                         <MessagesNotificationButton />
                         <NotificationBellButton />
 
-                        <ProfileDropdown session={session} /> */}
+                        <ProfileDropdown session={session} />
                     </div>
                 </Header>
                 {children}
