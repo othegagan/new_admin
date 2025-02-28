3539bb07db56318625958e559ef36b43779bbe12
othegagan <hellothegagan@gmail.com>
2025-02-28 14:10:54 +0530

    feat: Refactor host constraint functions to remove session dependency and update endpoint for pricing constraints

--- server/configurations.ts
+++ server/configurations.ts
@@ -1,26 +1,18 @@
 import { env } from '@/env';
 import { api } from '@/lib/apiService';
-import { getSession } from 'next-auth/react';
 
 const HOST_VEHICLE_SERVICES_BASEURL = env.NEXT_PUBLIC_HOST_VEHICLE_SERVICES_BASEURL;
 
 export async function getHostConstraint() {
-    const session = await getSession();
-    const url = `${HOST_VEHICLE_SERVICES_BASEURL}/v1/vehicle/getHostConstraint`;
-    const payload = {
-        fromValue: 'constraintnhostids',
-        hostIds: [session?.iduser],
-        constraintName: 'PriceConstraint'
-    };
+    const url = `${HOST_VEHICLE_SERVICES_BASEURL}/v1/vehicle/getHostPricingConstraint`;
+    const payload = {};
 
     const response = await api.post<any>(url, payload);
     return response;
 }
 
 export async function updateHostConfigurations(payload: any) {
-    const session = await getSession();
     const url = `${HOST_VEHICLE_SERVICES_BASEURL}/v1/vehicle/updateHostBusinessConstraint`;
-    payload.hostId = session?.iduser;
     const response = await api.post<any>(url, payload);
     return response;
 }
